418c57c023380d8424ece8ea791a80572354daec
Vitalii Morgunov <utlab13@gmail.com>
2024-08-21 22:29:54 +0200

    Add health check

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # shardem_health_check
+
+tmux new-session -d -s shardeum_healthcheck 'bash <(curl -s https://raw.githubusercontent.com/vmorgunov/main/shardeum_health_check.sh)'
---
+++ shardeum_health_check.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+function get_status() {
+    STATUS=$(docker exec -t shardeum-dashboard operator-cli status | grep state | awk '{ print $2 }' || echo "stopped")
+    echo "${STATUS}"
+}
+
+cd "$HOME" || exit
+source .profile
+while true
+do
+    printf "Check shardeum node status \n"
+    NODE_STATUS=$(get_status)
+    printf "Current status: ${NODE_STATUS}\n"
+    sleep 5s
+    if [[ "${NODE_STATUS}" =~ "stopped" ]]; then
+        docker-compose restart
+        (docker logs -f shardeum-dashboard &) | grep -q 'done' && sleep 20
+        printf "Start shardeum node and wait 5 minutes\n"
+        docker exec -it shardeum-dashboard operator-cli start
+        sleep 5m
+    else
+        date=$(date +"%H:%M")
+        echo "Last Update: ${date}"
+        printf "Sleep 15 minutes\n"
+        sleep 15m
+    fi
+done
